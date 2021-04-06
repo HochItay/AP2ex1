bea47d640c8b2a5dfd828978f1362e46f3b88c6a
HochItay <itayhoch1212@gmail.com>
2021-04-06 13:13:31 +0300

    first implementaion of IFlightModel, IFlightViewModel and FlightViewModel

---
+++ AP2ex1/Model/IFlightModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace AP2ex1.Model
+{
+    public interface IFlightModel : INotifyPropertyChanged
+    {
+        // video related properties
+        float VideoSpeed { get; set; }
+        int VideoLength { get; }
+        int VideoCurrentTime { get; set; }
+        bool VideoIsRunning { get; set; }
+
+        string ChosenProperty { get; set; }
+        bool VideoIsRunning { get; set; }
+        bool VideoIsRunning { get; set; }
+
+        // file loading methods
+        void LoadFlightDataFile(string filePath);
+        void LoadSettingsFile(string filePath);
+    }
+}
