d48ca9458d4a862aaa1d32c06c343bb66186a816
yoav amar <yoav.6hutc@gmail.com>
2021-04-11 22:15:49 +0300

    add vm for YPR displayer

---
+++ AP2ex1/controlersViewModel/IVMYPRDisplayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2ex1.controlersViewModel
+{
+    interface IVMYPRDisplayer
+    {
+        int Yaw
+        {
+            set;
+            get;
+        }
+        int Pitch
+        {
+            set;
+            get;
+        }
+        int Roll
+        {
+            set;
+            get;
+        }
+    }
+}
