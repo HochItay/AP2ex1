b6507daf0f3b76c941d1d796a897df9dbd637f9b
yoav amar <yoav.6hutc@gmail.com>
2021-04-11 21:48:18 +0300

    add the vm for the compass

---
+++ AP2ex1/controlersViewModel/IVMFlightData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2ex1.controlersViewModel
+{
+    interface IVMFlightData
+    {
+        int CompassAngle
+        {
+            set;
+            get;
+        }
+    }
+}
---
+++ AP2ex1/controlersViewModel/VMFlightData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2ex1.controlersViewModel
+{
+    class VMFlightData : IVMFlightData
+    {
+        private int compassAngle = 180;
+        public int CompassAngle
+        {
+            get
+            {
+                return compassAngle;
+            }
+            set
+            {
+                compassAngle = value;
+            }
+        }
+    }
+}
