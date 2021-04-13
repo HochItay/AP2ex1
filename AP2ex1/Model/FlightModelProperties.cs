d8f3da32cc51aeceb9212868a5e29a7860e0ee32
HochItay <itayhoch1212@gmail.com>
2021-04-13 22:41:54 +0300

    add properties

--- AP2ex1/Model/FlightModelProperties.cs
+++ AP2ex1/Model/FlightModelProperties.cs
@@ -8,9 +8,73 @@ namespace AP2ex1.Model
 {
     public partial class FlightModel : IFlightModel
     {
-        private readonly string[] properties =  { "hi" };
+        private readonly string[] properties =  { "CompassAngle", "Speed", "Height", "JoystickX", "JoystickY", "Yaw", "Pitch", "Roll"};
 
-        
+        public int CompassAngle
+        {
+            get 
+            {        
+                return (int) fp.GetPropertyAtLine("", currentLine); 
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return (int)fp.GetPropertyAtLine("", currentLine);
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return (int)fp.GetPropertyAtLine("", currentLine);
+            }
+        }
+
+        public int JoystickX
+        {
+            get
+            {
+                return (int)fp.GetPropertyAtLine("", currentLine);
+            }
+        }
+
+        public int JoystickY
+        {
+            get
+            {
+                return (int)fp.GetPropertyAtLine("", currentLine);
+            }
+        }
+
+        public int Yaw
+        {
+            get
+            {
+                return (int)fp.GetPropertyAtLine("", currentLine);
+            }
+        }
+
+        public int Pitch
+        {
+            get
+            {
+                return (int)fp.GetPropertyAtLine("", currentLine);
+            }
+        }
+
+        public int Roll
+        {
+            get
+            {
+                return (int)fp.GetPropertyAtLine("", currentLine);
+            }
+        }
+
+
         private void NotifyChanges()
         {
             foreach (string prop in properties)
