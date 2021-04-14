601b41d23d814aeb73fc68c5613e46d06fd74fda
NitayRe <nitay12332@gmail.com>
2021-04-14 18:20:04 +0300

    speed != comp-deg

--- AP2ex1/ViewModel/controlersViewModel/VMFlightData.cs
+++ AP2ex1/ViewModel/controlersViewModel/VMFlightData.cs
@@ -42,7 +42,7 @@ namespace AP2ex1.ViewModel
         {
             get
             {
-                return model.CompassAngle;
+                return model.Speed;
             }
         }
         public int VM_Height
