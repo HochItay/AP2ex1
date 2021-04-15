fae0abc2dff496058fa3a4e0dc8b8c6be4306c56
yoav amar <yoav.6hutc@gmail.com>
2021-04-15 22:58:27 +0300

    added notes to  view model

--- AP2ex1/ViewModel/framesViewModel/IVMPSimulator.cs
+++ AP2ex1/ViewModel/framesViewModel/IVMPSimulator.cs
@@ -8,10 +8,30 @@ namespace AP2ex1.ViewModel
 {
     public interface IVMPSimulator
     {
+        /// <summary>
+        /// return the VMFlightData.
+        /// </summary>
+        /// <returns></returns>
         IVMFlightData GetVMFlightData();
+        /// <summary>
+        /// return VMMediaController
+        /// </summary>
+        /// <returns></returns>
         IVMMediaController GetVMMediaController();
+        /// <summary>
+        /// return the VMYPRDisplayer.
+        /// </summary>
+        /// <returns></returns>
         IVMYPRDisplayer GetVMYPRDisplayer();
+        /// <summary>
+        /// return the VMGraphController
+        /// </summary>
+        /// <returns>VMGraphController</returns>
         VMGraphController GetVMGraphController();
+        /// <summary>
+        /// return the VMFlightControllers
+        /// </summary>
+        /// <returns>IVMFlightControllers</returns>
         IVMFlightControllers GetVMFlightControllers();
     }
 }
