c66345e09f6f6edae6e6b510ef00e5cf049d8f22
NADAV SHARABI <nadav.sharabi1@live.biu.ac.il>
2021-04-15 22:34:56 +0300

    adding notes

--- AP2ex1/ViewModel/controlersViewModel/AVMGraph.cs
+++ AP2ex1/ViewModel/controlersViewModel/AVMGraph.cs
@@ -11,14 +11,23 @@ using OxyPlot.Series;
 
 namespace AP2ex1.ViewModel
 {
+    /// <summary>
+    /// This is an abstruct class that is used
+    /// for saving code, in here you would find 
+    /// the shared code for specific classes.
+    /// </summary>
     abstract class AVMGraph : INotifyPropertyChanged
     {
         protected static readonly int START_POINT_INDEX = 0;
         protected Axis xAxe;
         protected Axis yAxe;
         private Model.IMGraph model;
-        private PlotModel plotModel;
         protected Thread thread;
+
+        /// <summary>
+        /// This is a the plot modle witch saves data on the graph.
+        /// </summary>
+        private PlotModel plotModel;
         public PlotModel PlotModel
         {
             get 
@@ -36,6 +45,9 @@ namespace AP2ex1.ViewModel
             }
         }
 
+        /// <summary>
+        /// This is the current line witch is fed to the flight geer.
+        /// </summary>
         public int VM_CurrentLine
         {
             get
@@ -44,63 +56,94 @@ namespace AP2ex1.ViewModel
             }
         }
 
+        /// <summary>
+        /// Constructor for shared intializations. 
+        /// </summary>
+        /// <param name="model">This is the model that gives the data to the graph</param>
         public AVMGraph(Model.IMGraph model)
         {
             PlotModel = new PlotModel();
+
+            //set shared data on graphs
             SetUpModel();
 
-            thread = new Thread(new ThreadStart(updateWithTread));
+            //adding thread to run updates on the graphs
+            thread = new Thread(new ThreadStart(updateWithThread));
+
+
             this.model = model;
-            /*
-            model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e) {
-                string varName = "VM_" + e.PropertyName;
-                if (varName.Equals("VM_CurrentLine")) {
-                    thread.Start();
-                }
-            };
-            */
         }
 
-        private void updateWithTread()
+        /// <summary>
+        /// Func to fed the thred that would updates the graph.
+        /// </summary>
+        private void updateWithThread()
         {
             while(true)
             {
-
                 UpdateGraphPoints();
+
+                //we wait 200 ms between updates
                 Thread.Sleep(200);
             }
         }
 
+        /// <summary>
+        /// Sets the graph's title
+        /// </summary>
+        /// <param name="title">the title to set </param>
         public void SetTitle(string title)
         {
             PlotModel.LegendTitle = title;
         }
 
+        /// <summary>
+        /// Sets the title of Y Axe
+        /// </summary>
+        /// <param name="title">the title to set</param>
         public void SetYTitle(string title)
         {
             yAxe.Title = title;
             PlotModel.InvalidatePlot(true);
         }
 
+        /// <summary>
+        /// Sets the title of X Axe
+        /// </summary>
+        /// <param name="title">the title to set</param>
         public void SetXTitle(string title)
         {
             xAxe.Title = title;
             PlotModel.InvalidatePlot(true);
         }
 
+        /// <summary>
+        /// Sets data on the graph
+        /// </summary>
         private void SetUpModel()
         {
+            //Sets the look of the Graph title
             PlotModel.LegendTitleFontSize = 15;
             PlotModel.LegendOrientation = LegendOrientation.Horizontal;
             PlotModel.LegendPlacement = LegendPlacement.Outside;
             PlotModel.LegendPosition = LegendPosition.TopCenter;
 
+            //Sets Y Axe of the graph
             yAxe = new LinearAxis() {Position = AxisPosition.Left};
             PlotModel.Axes.Add(yAxe);
         }
-        public abstract void UpdateGraphPoints();
 
+        /// <summary>
+        /// updates the graphs point. 
+        /// </summary>
+        public abstract void UpdateGraphPoints();
+        
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies this PropertyChanged with the propety that changed
+        /// </summary>
+        /// <param name="propertyName"></param>
         protected virtual void NotifyPropertyChanged(string propertyName)
         {
             PropertyChangedEventHandler handler = PropertyChanged;
--- AP2ex1/ViewModel/controlersViewModel/VMLinesGraph.cs
+++ AP2ex1/ViewModel/controlersViewModel/VMLinesGraph.cs
@@ -10,53 +10,78 @@ using System.Windows;
 
 namespace AP2ex1.ViewModel
 {
+    /// <summary>
+    /// represents graph that conects dots with lines
+    /// </summary>
     class VMLinesGraph: AVMGraph
     {
         private List<DataPoint> allPoints;
         private LineSeries displayedPoints;
         private bool isDataIntialized = false;
 
+        /// <summary>
+        /// Constructor for this graph type.
+        /// </summary>
+        /// <param name="model">the model to get the data from</param>
         public VMLinesGraph(Model.IMGraph model) : base(model)
         {
+            //sets the X Axe
             xAxe = new TimeSpanAxis() { Position = AxisPosition.Bottom, StringFormat = "mm:ss"};
             PlotModel.Axes.Add(xAxe);
-
         }
 
+        /// <summary>
+        /// Sets the graph's data
+        /// </summary>
+        /// <param name="allPoints">all the point that in the graph</param>
         public void SetGraphData(IList<Point> allPoints)
         {
+            //clear the graph
             PlotModel.Series.Clear();
 
+            //adds the sereis of the displayed points on the graph
             this.allPoints = GetDataPointList(allPoints);
             displayedPoints = new LineSeries() {ItemsSource = this.allPoints.GetRange(START_POINT_INDEX, START_POINT_INDEX)};
-
             PlotModel.Series.Add(displayedPoints);
 
+            //only the first time the graph is set we should start the update thread
             if (!isDataIntialized)
             {
                 thread.Start();
                 isDataIntialized = true;
             }
         }
+
+        /// <summary>
+        /// Gets the point in the right object
+        /// </summary>
+        /// <param name="allPoints">all the graphs points</param>
+        /// <returns></returns>
         private List<DataPoint> GetDataPointList(IList<Point> allPoints)
         {
             List<DataPoint> dataPoints = new List<DataPoint>();
 
             foreach(Point point in allPoints)
             {
+                //convert all point to type of DataPoint
                 dataPoints.Add(new DataPoint(point.X, point.Y));
             }
 
             return dataPoints;
         }
 
+        /// <summary>
+        /// updates the graphs points.
+        /// </summary>
         public override void UpdateGraphPoints()
         {
+            //if the graph isn't intialized shouldn't update.
             if (!isDataIntialized)
             {
                 return;
             }
 
+            //Updates the points to display.
             displayedPoints.ItemsSource = allPoints.GetRange(START_POINT_INDEX, VM_CurrentLine);
             PlotModel.InvalidatePlot(true);
         }
