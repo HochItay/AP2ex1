fc085500f7c60657f3fb482c67e86891d00533ab
yoav amar <yoav.6hutc@gmail.com>
2021-04-15 22:12:06 +0300

    added notes to  model

--- AP2ex1/Model/IMMain.cs
+++ AP2ex1/Model/IMMain.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace AP2ex1.Model
 {
     /// <summary>
-    /// this interface is an extention of all the smaller vm interfaces. 
+    /// this interface is an extention of all the smaller model interfaces. 
     /// </summary>
     public interface IMMain : IMPGetFiles, IMPSimulator
     {
--- AP2ex1/Model/framesIModel/IMPSimulator.cs
+++ AP2ex1/Model/framesIModel/IMPSimulator.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace AP2ex1.Model
 {
     /// <summary>
-    /// this interface is an extention of all the vm interfaces in the simulator page.
+    /// this interface is an extention of all the model interfaces in the simulator page.
     /// </summary>
     public interface IMPSimulator : IMYPRDisplayer, IMGraphController, IMMediaController, IMFlightData, IMFlightControllers
     {
