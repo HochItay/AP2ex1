964815f691e9903df53c59932f08b0cb02e34576
NADAV SHARABI <nadav.sharabi1@live.biu.ac.il>
2021-04-15 18:23:40 +0300

    changing interface

--- AP2ex1/Model/IControlersModel/IMGraphController.cs
+++ AP2ex1/Model/IControlersModel/IMGraphController.cs
@@ -16,7 +16,7 @@ namespace AP2ex1.Model
         int GetNumPointsPerSec();
         string GetCorrelativeVar(string var);
         IList<Point> GetVarPoints(string var);
-        Tuple<IList<Point>, IList<Point>> GetAnomalyGraphPoints(string var, string corrilativeVar);
+        Tuple<IList<Point>, IList<Tuple<int, Point>>> GetAnomalyGraphPoints(string var, string corrilativeVar);
         IList<Tuple<Func<double, double>, double, double>> GetGraphFuncs(string var);
     }
 }
